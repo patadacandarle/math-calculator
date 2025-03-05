c7f07f80909ad2a0598c9101259de77a03b43027
patadacandarle <patadacandarle@gmail.com>
2025-03-05 22:10:32 +0000

    Add 1.php

---
+++ src/1.php
@@ -0,0 +1,21 @@
+<?php 
+// Function to calculate the sum of two numbers
+function sum($a, $b) {
+return $a + $b;
+}
+
+// Function to calculate the difference of two numbers
+function diff($a, $b) {
+return $a - $b;
+}
+
+// Function to calculate the product of two numbers
+function prod($a, $b) {
+return $a * $b;
+}
+
+// Function to calculate the quotient of two numbers
+function quot($a, $b) {
+return $a / $b;
+}
+?>
