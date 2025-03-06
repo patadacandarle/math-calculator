5147876d45e0c89b6e64413aebb6baad59bd867e
patadacandarle <patadacandarle@gmail.com>
2025-03-06 18:07:35 +0000

    Add 2.php

---
+++ src/2.php
@@ -0,0 +1,12 @@
+<?php
+// Random PHP code
+$x = 5;
+$y = 10;
+if ($x > $y) {
+    echo "X is greater than Y";
+} else if ($x == $y) {
+    echo "X is equal to Y";
+} else {
+    echo "X is less than Y";
+}
+?>
