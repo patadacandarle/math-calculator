cb9d5c9c25b731b1132f839e55f524616309b232
patadacandarle <patadacandarle@gmail.com>
2025-03-15 15:46:45 +0000

    Add 11.php

---
+++ src/11.php
@@ -0,0 +1,22 @@
+<?php
+
+function add($a, $b) {
+    return $a + $b;
+}
+
+function subtract($a, $b) {
+    return $a - $b;
+}
+
+function multiply($a, $b) {
+    return $a * $b;
+}
+
+function divide($a, $b) {
+    if ($b == 0) {
+        throw new \Exception('Cannot divide by zero');
+    }
+    return $a / $b;
+}
+
+?>
