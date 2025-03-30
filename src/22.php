e1884653daa31f02d879d2df17acd21f2f1c74e7
patadacandarle <patadacandarle@gmail.com>
2025-03-30 08:32:11 +0000

    Add 22.php

---
+++ src/22.php
@@ -0,0 +1,12 @@
+<?php
+// Define variables
+$number1 = 3;
+$operation = "add";
+$number2 = 5;
+
+// Perform calculation based on operation and numbers
+$result = ($number1 + $number2) . $operation;
+
+// Output result to user
+echo "$result";
+?>
