49926a8418260a573b28829fcbe91a1ec31b351e
patadacandarle <patadacandarle@gmail.com>
2025-04-03 12:35:39 +0000

    Add 26.php

---
+++ src/26.php
@@ -0,0 +1,10 @@
+<?php
+// Define the variables and expressions
+$var1 = 5;
+$operation = '+';
+$var2 = 3;
+$result = $var1 + $var2;
+
+// Use PHP's printf or print statement for output
+echo "The result of $var1 plus $var2 is: $result";
+?>
