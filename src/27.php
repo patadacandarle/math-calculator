90d7ea018021fff4e8c1c81f0628869e9c721d5d
patadacandarle <patadacandarle@gmail.com>
2025-04-05 05:09:56 +0000

    Add 27.php

---
+++ src/27.php
@@ -0,0 +1,11 @@
+<?php
+// Define variables and constants
+$var1 = "value1";
+$var2 = "value2";
+
+// Start of the PHP code
+
+// You can now use the variables $var1 and $var2 in your script
+
+// End of the PHP code
+?>
