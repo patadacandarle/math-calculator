fdcba951f510f9fdafcc91005513e41675248ddb
patadacandarle <patadacandarle@gmail.com>
2025-04-09 00:35:12 +0000

    Add 32.php

---
+++ src/32.php
@@ -0,0 +1,12 @@
+<?php
+// Assuming you want to use PHP for this example
+
+function add($a, $b) {
+    return $a + $b;
+}
+
+$firstNumber = 10;
+$secondNumber = 20;
+
+$result = add($firstNumber, $secondNumber);
+echo "The result of adding $firstNumber and $secondNumber is: $result";
