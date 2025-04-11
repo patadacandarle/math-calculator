bc4094a643ec0c1aaf47e988e5dc829320108b8d
patadacandarle <patadacandarle@gmail.com>
2025-04-11 11:57:45 +0000

    Add 33.php

---
+++ src/33.php
@@ -0,0 +1,12 @@
+<?php
+// Initialize variables
+$num1 = 5;
+$op = '+';
+$num2 = 7;
+
+// Perform arithmetic operation
+$result = $num1 + $num2 * $op;
+
+// Output result
+echo "The sum is: " . $result;
+?>
